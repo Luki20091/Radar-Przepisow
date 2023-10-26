7d0a2e1f6b2157427c7ab6e2a4482cb67825b299
Luki20091 <76406872+Luki20091@users.noreply.github.com>
2023-10-26 08:54:32 +0200

    Update locations.js

--- assets/scripts/locations.js
+++ assets/scripts/locations.js
@@ -1,26 +1,4 @@
-var locations = document.querySelectorAll('.button');
 
-locations.forEach(function (loc, index) {
-	loc.addEventListener('click', function (e) {
-		if (e.target.id == "") {
-			if (e.target.parentElement.id == "index") {
-				location.href = "index.html";
-			} else {
-				location.href = e.target.parentElement.id + ".html"
-			};
-		} else if (e.target.id == "index") {
-			location.href = "index.html";
-		} else {
-			location.href = e.target.id + ".html"
-		};
-	});
-});
-
-
-
-
-
-/*
 var locations = document.querySelectorAll('.button');
 
 locations.forEach(function (loc, index) {
@@ -38,6 +16,3 @@ locations.forEach(function (loc, index) {
 		};
 	});
 });
-*/
-
-
