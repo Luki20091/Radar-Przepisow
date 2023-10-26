92bd4334b8c392a29424ac126a531be3b8b3af73
Luki20091 <76406872+Luki20091@users.noreply.github.com>
2023-10-26 09:56:48 +0200

    Update locations.js

--- assets/scripts/locations.js
+++ assets/scripts/locations.js
@@ -5,14 +5,14 @@ locations.forEach(function (loc, index) {
 	loc.addEventListener('click', function (e) {
 		if (e.target.id == "") {
 			if (e.target.parentElement.id == "index") {
-				location.replace('https://luki20091.github.io/RadioZST');
+				location.replace('https://luki20091.github.io/Radar-Przepisow');
 			} else {
-				location.replace('https://luki20091.github.io/RadioZST/' + e.target.parentElement.id);
+				location.replace('https://luki20091.github.io/Radar-Przepisow/' + e.target.parentElement.id);
 			};
 		} else if (e.target.id == "index") {
-			location.replace('https://luki20091.github.io/RadioZST');
+			location.replace('https://luki20091.github.io/Radar-Przepisow');
 		} else {
-			location.replace('https://luki20091.github.io/RadioZST/' + e.target.id);
+			location.replace('https://luki20091.github.io/Radar-Przepisow/' + e.target.id);
 		};
 	});
 });
